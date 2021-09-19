6ab8d61a29e00f834759920fd67baa1d34642c89
madhusudhanarava <madhusudhan.arava@ibexlabs.com>
2021-09-19 23:26:09 +0530

    adding git files to workflow plan to hashicorp sandbox workflo

--- environments/production/variables.tf
+++ environments/production/variables.tf
@@ -1,31 +1,32 @@
-variable "env" {
-  default = "undefined"
-}
-
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "zones" {
-  default = ["a", "b", "c"]
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-
-# variable "public_subnet_cidr_blocks" {
-#   default = {
-#     zone0 = "10.0.10.0/24"
-#     zone1 = "10.0.20.0/24"
-#     zone2 = "10.0.30.0/24"
-#   }
+# variable "env" {
+#   default = "undefined"
 # }
 
-# variable "private_subnet_cidr_blocks" {
-#   default = {
-#     zone0 = "10.0.11.0/24"
-#     zone1 = "10.0.21.0/24"
-#     zone2 = "10.0.31.0/24"
-#   }
+# variable "region" {
+#   default = "us-east-1"
 # }
+
+# variable "zones" {
+#   default = ["a", "b", "c"]
+# }
+
+# variable "vpc_cidr" {
+#   default = "10.0.0.0/16"
+# }
+
+# # variable "public_subnet_cidr_blocks" {
+# #   default = {
+# #     zone0 = "10.0.10.0/24"
+# #     zone1 = "10.0.20.0/24"
+# #     zone2 = "10.0.30.0/24"
+# #   }
+# # }
+
+# # variable "private_subnet_cidr_blocks" {
+# #   default = {
+# #     zone0 = "10.0.11.0/24"
+# #     zone1 = "10.0.21.0/24"
+# #     zone2 = "10.0.31.0/24"
+# #   }
+# # }
+./environments/modules/variables.tf
