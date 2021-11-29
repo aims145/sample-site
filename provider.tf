ee428716f66b2fbfd2546e64cf7f15f20640ba28
madhusudhanarava <madhusudhan.arava@ibexlabs.com>
2021-11-29 13:42:33 +0530

    updated tf files

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAXDSL7DJCRCBXV7E2"
-  secret_key = "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"
+  access_key = "AKIAXDSL7DJCYKZR6MFU"
+  secret_key = "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"
 }
